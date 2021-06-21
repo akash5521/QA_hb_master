485846f856c15b1983a2008fb13e370c65906c60
akash5521 <kashyapakash0607@gmail.com>
2021-06-21 22:58:25 +0530

    implemented extends report & data paremetrization

--- eclipse-workspace/HbUnifiedAutomation/src/test/java/UnifiedApp/HbUnifiedAutomation/EnvSetup.java
+++ eclipse-workspace/HbUnifiedAutomation/src/test/java/UnifiedApp/HbUnifiedAutomation/EnvSetup.java
@@ -14,15 +14,15 @@ import io.appium.java_client.internal.Config;
 
 public class EnvSetup extends Base
 {
-@Test
-public void selectServer() throws IOException, InterruptedException
+@Test(dataProvider = "InputData",dataProviderClass = DataParametrisation.class)
+public void selectServer(String data) throws IOException, InterruptedException
 {
 	service = StartServer();
 	AndroidDriver<AndroidElement> driver = capabilities("UnifiedApp");
 	driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 	Select_Enviroment se = new Select_Enviroment(driver);
 	se.getSelect_Enviroment().click();
-	se.getcompany_name().sendKeys("eatgood");
+	se.getcompany_name().sendKeys(data);
 	se.getnext_btn().click();
 	
 	
